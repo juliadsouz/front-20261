ecd1ddc8b6a17968082978f2ea5714d1133f1018
Júlia de Souza <juliadsouz2@gmail.com>
2026-03-24 10:56:15 -0300

    Codigo da aula02

--- aulas/aula02/src/App.jsx
+++ aulas/aula02/src/App.jsx
@@ -1,14 +1,19 @@
 import './App.css';
+import logo from './assets/learn.svg';
 
 function App (){
   return (
-  <div>
-    <header> </header>
-    <main> </main>
-    <footer> </footer>
-
-
-  </div>
+    <main>
+    <img src={logo} alt='Logotipo da aplicação' />
+    <h1>Aluno Online</h1>
+    <label htmlfor="matricula">Matricula</label>
+    <input type="number" id="matricula" 
+      name="matricula"/>
+    <label for="senha">Senha</label>
+    <input type="password" id="senha"
+      name="senha"/>
+      <button type="submit">Entrar</button>
+  </main>
   );
 }
 export default App;
