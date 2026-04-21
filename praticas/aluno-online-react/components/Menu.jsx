21692ff87342010ab9690f55ff5c7ceea70e4e90
Júlia de Souza <juliadsouz2@gmail.com>
2026-04-21 14:00:32 -0300

    feat: corrigindo sintaxe

--- praticas/aluno-online-react/components/Menu.jsx
+++ praticas/aluno-online-react/components/Menu.jsx
@@ -4,12 +4,12 @@ function Menu({ navegaPara }) {
   return (
     <nav>
       <ul>
-        <li><a href="#" onClick={(e) => navegaPara(1)}>Dashboard</a></li>
-        <li><a href="#" onClick={(e) => navegaPara(2)}>Notas</a></li>
-        <li><a href="#" onClick={(e) => navegaPara(3)}>Faltas</a></li>
-        <li><a href="#" onClick={(e) => navegaPara(4)}>Boletos</a></li>
-        <li><a href="#" onClick={(e) => navegaPara(5)}>Requerimentos</a></li>
-        <li><a href="#" onClick={(e) => navegaPara(0)}>Sair</a></li>
+        <li><a href="#" onClick={() => navegaPara(1)}>Dashboard</a></li>
+        <li><a href="#" onClick={() => navegaPara(2)}>Notas</a></li>
+        <li><a href="#" onClick={() => navegaPara(3)}>Faltas</a></li>
+        <li><a href="#" onClick={() => navegaPara(4)}>Boletos</a></li>
+        <li><a href="#" onClick={() => navegaPara(5)}>Requerimentos</a></li>
+        <li><a href="#" onClick={() => navegaPara(0)}>Sair</a></li>
       </ul>
     </nav>
   );
